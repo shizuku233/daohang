0ce36e8a3d9a7d04d79d87abdc7666b41ffebba3
shizuku233 <65892478+shizuku233@users.noreply.github.com>
2022-06-23 12:52:15 +0800

    Update sites.js

--- JavaScript/sites.js
+++ JavaScript/sites.js
@@ -3,258 +3,258 @@ var sites = {
         [{
             "href": "https://dwnld.aicp-rom.com/",
             "icon": "https://dwnld.aicp-rom.com/imgs/favicons/favicon.ico",
-            "name": "AICP",
+            "title": "AICP",
         }, {
             "href": "https://arrowos.net/",
             "icon": "https://arrowos.net/img/logo.png",
-            "name": "ArrowOS",
+            "title": "ArrowOS",
         }, {
             "href": "https://get.carbonrom.org/",
             "icon": "https://get.carbonrom.org/assets/favicon/favicon.ico",
-            "name": "CarbonROM",
+            "title": "CarbonROM",
         }, {
             "href": "https://crdroid.net/",
             "icon": "./images/icons/crdroid.ico",
-            "name": "crdroid",
+            "title": "crdroid",
         }, {
             "href": "https://download.havoc-os.com/",
             "icon": "https://download.havoc-os.com/app/assets/images/logo.svg",
-            "name": "Havoc-OS",
+            "title": "Havoc-OS",
         }, {
             "href": "https://download.lineageos.org/",
             "icon": "https://download.lineageos.org/static/favicons/favicon.ico",
-            "name": "LineageOS",
+            "title": "LineageOS",
         }, {
             "href": "https://download.mokeedev.com/",
             "icon": "https://download.mokeedev.com/download-assets/images/favicon/favicon.ico",
-            "name": "Mokee",
+            "title": "Mokee",
         }, {
             "href": "https://download.pixelexperience.org/",
             "icon": "./images/icons/pixelexperience.png",
-            "name": "pixelexperience",
+            "title": "pixelexperience",
         }],
         [{
             "href": "https://orangefox.download/",
             "icon": "https://orangefox.download/favicon.ico",
-            "name": "OrangeFox",
+            "title": "OrangeFox",
         }, {
             "href": "https://twrp.me/",
             "icon": "https://twrp.me/favicon.ico",
-            "name": "TWRP",
+            "title": "TWRP",
         }],
     ],
     "机场": [
         [{
             "href": "https://3.akkcloud1.com/",
             "icon": "https://3.akkcloud1.com/favicon.ico",
-            "name": "Akkcloud",
+            "title": "Akkcloud",
         }, {
             "href": "https://dukou.dev/",
             "icon": "https://dukou.dev/logo.ico",
-            "name": "Dukou",
+            "title": "Dukou",
         }],
     ],
     "教程": [
         [{
             "href": "https://www.runoob.com/",
             "icon": "https://static.runoob.com/images/favicon.ico",
-            "name": "菜鸟教程",
+            "title": "菜鸟教程",
         }, {
             "href": "https://www.w3school.com.cn/",
             "icon": "https://www.w3school.com.cn/ui2019/logo-32-red.png",
-            "name": "W3School",
+            "title": "W3School",
         }],
     ],
     "论坛": [
         [{
             "href": "https://galge.fun/",
             "icon": "https://galge.fun/favicon.ico",
-            "name": "2DFan",
+            "title": "2DFan",
         }, {
             "href": "https://bangumi.tv/",
             "icon": "https://bangumi.tv/img/favicon.ico",
-            "name": "Bangumi",
+            "title": "Bangumi",
         }, {
             "href": "https://seiya-saiga.com/",
             "icon": "https://seiya-saiga.com/image/favicon.ico",
-            "name": "誠也の部屋",
+            "title": "誠也の部屋",
         }, {
             "href": "https://bbs.9shenmi.com/",
             "icon": "https://bbs.9shenmi.com/favicon.ico",
-            "name": "绯月",
+            "title": "绯月",
         }, {
             "href": "https://www.sayhuahuo.com/",
             "icon": "https://www.sayhuahuo.com/favicon.ico",
-            "name": "花火学园",
+            "title": "花火学园",
         }, {
             "href": "https://www.hacg.cat/",
             "icon": "https://www.hacg.cat/favicon.ico",
-            "name": "琉璃神社",
+            "title": "琉璃神社",
         }, {
             "href": "http://www.acgjc.com/",
             "icon": "./images/icons/漫音社.ico",
-            "name": "漫音社",
+            "title": "漫音社",
         }, {
             "href": "https://bbs.mikocon.com/",
             "icon": "https://bbs.mikocon.com/favicon.ico",
-            "name": "Mikocon",
+            "title": "Mikocon",
         }, {
             "href": "https://www.okloli.com/",
             "icon": "https://www.okloli.com/favicon.ico",
-            "name": "MyGalgame",
+            "title": "MyGalgame",
         }, {
             "href": "https://www.east-plus.net/",
             "icon": "https://www.east-plus.net/favicon.ico",
-            "name": "南+",
+            "title": "南+",
         }, {
             "href": "https://www.aigalgame.com/",
             "icon": "./images/icons/绅士天堂.ico",
-            "name": "绅士天堂",
+            "title": "绅士天堂",
         }, {
             "href": "https://www.tiangal.com/",
             "icon": "https://www.tiangal.com/wp-content/uploads/2022/04/2022042217411468.png",
-            "name": "天遊二次元",
+            "title": "天遊二次元",
         }, {
             "href": "https://vndb.org/",
             "icon": "https://vndb.org/favicon.ico",
-            "name": "VNDB",
+            "title": "VNDB",
         }, {
             "href": "https://www.yygal.com/",
             "icon": "https://www.yygal.com/static/images/icon.jpg",
-            "name": "XXGalgame",
+            "title": "XXGalgame",
         }, {
             "href": "https://www.ymgal.com/",
             "icon": "https://www.ymgal.com/favicon.ico",
-            "name": "月幕Galgame",
+            "title": "月幕Galgame",
         }, {
             "href": "https://bbs.zdfx.net/",
             "icon": "https://bbs.zdfx.net/img/zdfx.svg",
-            "name": "终点",
+            "title": "终点",
         }],
         [{
             "href": "https://www.423down.com/",
             "icon": "https://www.423down.com/wp-content/themes/D7/img/favicon.ico",
-            "name": "423Down",
+            "title": "423Down",
         }, {
             "href": "https://greasyfork.org/",
             "icon": "https://greasyfork.org/packs/media/images/blacklogo16-5421a97c75656cecbe2befcec0778a96.png",
-            "name": "Greasy Fork",
+            "title": "Greasy Fork",
         }, {
             "href": "https://next.itellyou.cn/",
             "icon": "https://next.itellyou.cn/favicon.ico",
-            "name": "ITELLYOU",
+            "title": "ITELLYOU",
         }, {
             "href": "https://forum.mobilism.org/",
             "icon": "https://forum.mobilism.org/favicon.ico",
-            "name": "Mobilism",
+            "title": "Mobilism",
         }, {
             "href": "https://rsload.net/",
             "icon": "https://rsload.net/favicon.ico",
-            "name": "rsload",
+            "title": "rsload",
         }, {
             "href": "https://forum.xda-developers.com/",
             "icon": "https://forum.xda-developers.com/data/assets/logo/favicon-32x32.png",
-            "name": "XDA",
+            "title": "XDA",
         }],
         [{
             "href": "https://www.expreview.com/",
             "icon": "https://cj.expreview.com/favicon.ico",
-            "name": "超能网",
+            "title": "超能网",
         }, {
             "href": "https://www.landiannews.com/",
             "icon": "https://static.lancdn.com/landian/v4.0.1/images/favicon.png",
-            "name": "蓝点网",
+            "title": "蓝点网",
         }],
     ],
     "商城": [
         [{
             "href": "https://store.epicgames.com/",
             "icon": "https://static-assets-prod.epicgames.com/epic-store/static/favicon.ico",
-            "name": "EPIC",
+            "title": "EPIC",
         }, {
             "href": "https://www.dlsite.com/",
             "icon": "https://www.dlsite.com/images/web/common/favicon.ico",
-            "name": "DLsite",
+            "title": "DLsite",
         }, {
             "href": "https://www.jd.com/",
             "icon": "https://www.jd.com/favicon.ico",
-            "name": "京东",
+            "title": "京东",
         }, {
             "href": "https://www.taobao.com/",
             "icon": "https://www.taobao.com/favicon.ico",
-            "name": "淘宝",
+            "title": "淘宝",
         }],
     ],
     "社交": [
         [{
             "href": "https://tieba.baidu.com/",
             "icon": "https://tb3.bdstatic.com/public/icon/favicon-v2.ico",
-            "name": "百度贴吧",
+            "title": "百度贴吧",
         }, {
             "href": "https://twitter.com/",
             "icon": "https://abs.twimg.com/favicons/twitter.2.ico",
-            "name": "Twitter",
+            "title": "Twitter",
         }, {
             "href": "https://weibo.com/",
             "icon": "https://weibo.com/favicon.ico",
-            "name": "微博",
+            "title": "微博",
         }],
     ],
     "搜索": [
         [{
             "href": "https://www.baidu.com/",
             "icon": "https://www.baidu.com/favicon.ico",
-            "name": "百度",
+            "title": "百度",
         }, {
             "href": "https://www.google.com/",
             "icon": "https://www.google.com/favicon.ico",
-            "name": "Google",
+            "title": "Google",
         }],
     ],
     "网盘": [
         [{
             "href": "https://www.123pan.com/",
             "icon": "https://www.123pan.com/static/favicon.ico",
-            "name": "123云盘",
+            "title": "123云盘",
         }, {
             "href": "https://www.aliyundrive.com/",
             "icon": "https://gw.alicdn.com/imgextra/i3/O1CN01aj9rdD1GS0E8io11t_!!6000000000620-73-tps-16-16.ico",
-            "name": "阿里云盘",
+            "title": "阿里云盘",
         }, {
             "href": "https://pan.baidu.com/",
             "icon": "https://nd-static.bdstatic.com/m-static/v20-main/favicon-main.ico",
-            "name": "百度网盘",
+            "title": "百度网盘",
         }, {
             "href": "https://pc.woozooo.com/",
             "icon": "https://pc.woozooo.com/favicon.ico",
-            "name": "蓝奏云",
+            "title": "蓝奏云",
         }],
     ],
     "影音": [
         [{
             "href": "https://www.acfun.cn/",
             "icon": "https://cdn.aixifan.com/ico/favicon.ico",
-            "name": "AcFun",
+            "title": "AcFun",
         }, {
             "href": "https://www.agemys.com/",
             "icon": "https://www.agemys.com/favicon.ico",
-            "name": "AGE动漫",
+            "title": "AGE动漫",
         }, {
             "href": "https://www.asmr.one/",
             "icon": "https://www.asmr.one/statics/icons/favicon.ico",
-            "name": "ASMR Online",
+            "title": "ASMR Online",
         }, {
             "href": "https://www.bilibili.com/",
             "icon": "https://www.bilibili.com/favicon.ico",
-            "name": "哔哩哔哩",
+            "title": "哔哩哔哩",
         }, {
             "href": "https://www.pixiv.net/",
             "icon": "https://www.pixiv.net/favicon.ico",
-            "name": "Pixiv",
+            "title": "Pixiv",
         }, {
             "href": "https://music.163.com/",
             "icon": "https://s1.music.126.net/style/favicon.ico",
-            "name": "网易云音乐",
+            "title": "网易云音乐",
         }],
     ],
 }
