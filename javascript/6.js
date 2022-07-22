2235574c0e63220068e783dbed475312f3a8a54b
shizuku233 <2860481957@qq.com>
2022-07-22 15:43:06 +0800

    Update 6.js

--- javascript/6.js
+++ javascript/6.js
@@ -25,24 +25,28 @@ function openurl() {
 }
 
 const FORMSET = {
-    "2DFan": {action: "https://galge.fun/subjects/search", name: "keyword", method: ""},
-    "百度": {action: "https://www.baidu.com/s", name: "wd", method: ""},
-    "Bangumi": {action: "https://bangumi.tv/subject_search", name: "search_text", method: "post"},
-    "必应": {action: "https://www.bing.com/search", name: "q", method: ""},
-    "Google": {action: "https://www.google.com/search", name: "q", method: ""},
-    "MyGalgame": {action: "https://www.okloli.com", name: "s", method: ""},
-    "VNDB": {action: "https://vndb.org/v", name: "sq", method: ""},
+    "2DFan": {action: "https://galge.fun/subjects/search", name: "keyword", method: "", image: "https://galge.fun/favicon.ico"},
+    "百度": {action: "https://www.baidu.com/s", name: "wd", method: "", image: "https://www.baidu.com/favicon.ico"},
+    "Bangumi": {action: "https://bangumi.tv/subject_search", name: "search_text", method: "post", image: "https://bangumi.tv/img/favicon.ico"},
+    "必应": {action: "https://www.bing.com/search", name: "q", method: "", image: "https://www.bing.com/sa/simg/favicon-trans-bg-blue-mg.ico"},
+    "Google": {action: "https://www.google.com/search", name: "q", method: "", image: "https://www.google.com/favicon.ico"},
+    "MyGalgame": {action: "https://www.okloli.com", name: "s", method: "", image: "https://www.okloli.com/favicon.ico"},
+    "VNDB": {action: "https://vndb.org/v", name: "sq", method: "", image: "https://vndb.org/favicon.ico"},
 };
 
 window.onload = function () {
     let search = document.querySelector("#search");
+    //let dropdown_menu = "";
+    //for (let form in FORMSET) {
+    //    dropdown_menu += "<span><img src='" + FORMSET[form].image + "'><span>" + form + "</span></span>";
+    //}
+    //search.querySelector(".dropdown-menu").innerHTML = dropdown_menu;
     search.querySelectorAll(".dropdown-menu > span").forEach(function (span) {
         span.addEventListener("click", function () {
-            let img = span.querySelector("img");
             let name = span.querySelector("span").textContent;
             search.setAttribute("action", FORMSET[name].action);
             search.setAttribute("method", FORMSET[name].method);
-            search.querySelector("div > img").src = img.src;
+            search.querySelector("div > img").src = FORMSET[name].image;
             search.querySelector("input").name = FORMSET[name].name;
         })
     })
