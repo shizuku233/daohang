53f373aff226286a0e97ae393d124c3aac5f1c82
shizuku233 <65892478+shizuku233@users.noreply.github.com>
2024-10-16 17:00:47 +0800

    Update site.js

--- js/site.js
+++ js/site.js
@@ -61,8 +61,9 @@ const NAVSITE = {
     "no-2": [
         {href: "https://github.com/BiTGApps/BiTGApps-Release/releases/", image: "https://github.githubassets.com/favicons/favicon.svg", title: "BiTGApps"},
         {href: "https://sourceforge.net/projects/flamegapps/files/arm64/", image: "https://a.fsdn.com/con/img/sandiego/svg/originals/sf-icon-orange-no_sf.svg", title: "FlameGApps"},
+        {href: "https://sourceforge.net/projects/litegapps/files/", image: "https://a.fsdn.com/allura/p/litegapps/icon?w=90", title: "LiteGapps"},
         {href: "https://sourceforge.net/projects/nikgapps/files/Releases/", image: "https://a.fsdn.com/con/img/sandiego/svg/originals/sf-icon-orange-no_sf.svg", title: "NikGApps"},
-        {href: "https://opengapps.org/", image: "https://opengapps.org/images/android-icon-144x144.png", title: "Open GApps"},
+        {href: "https://sourceforge.net/projects/opengapps/files/", image: "https://a.fsdn.com/allura/p/opengapps/icon?w=90", title: "OpenGApps"},
     ],
     "工具": [
         {href: "https://www.ventusky.com/", image: "https://cdnstatic.ventusky.com/images_v2/favicon.ico", title: "Ventusky"},
