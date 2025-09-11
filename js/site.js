7b128345d2faafa9d2b3e6ed2edcf0e198ebedbb
shizuku233 <65892478+shizuku233@users.noreply.github.com>
2025-09-11 22:06:57 +0800

    Update site.js

--- js/site.js
+++ js/site.js
@@ -203,6 +203,7 @@ const NAVSITE = {
         {href: "https://coserlab.io/", image: "https://coserlab.io/wp-includes/images/w-logo-blue-white-bg.png", title: "Coser Lab"},
         {href: "https://www.coser.wang/", image: "https://www.coser.wang/favicon.ico", title: "Coser社区"},
         {href: "https://www.cos5.com/", image: "images/icons/globe.svg", title: "COS屋"},
+        {href: "https://cosz.com/", image: "https://cosz.com/favicon.ico", title: "CosZ"},
         {href: "https://www.moely.link/", image: "https://www.moely.link/assets/img/favicon.png", title: "萌哩"},
         {href: "https://www.pixiv.net/", image: "https://www.pixiv.net/favicon.ico", title: "Pixiv"},
         {href: "https://www.someacg.top/", image: "https://www.someacg.top/favicon.png", title: "SomeACG"},
