b241b14976a11b9fb157e2d5f29b92b531d2d401
shizuku233 <65892478+shizuku233@users.noreply.github.com>
2025-09-19 16:04:26 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function setSuggestionList(suggestionList) {
         div += "<li>" + "<span>" + suggestion + "</span><span></span>" + "</li>"
         search.querySelector(".suggestion > div").innerHTML = div
     }
-    search.querySelectorAll(".suggestion > div > li").forEach((li) => {
+    search.querySelectorAll(".suggestion > div > li").forEach(li => {
         li.children[0].addEventListener("click", () => {
             input.value = li.textContent
             search.submit()
@@ -152,24 +152,33 @@ input.addEventListener("focus", () => {
 function creatNav1() {
     let section = document.querySelector("section")
     for (let category in NAVSITE) {
-        let nav_site_bar = document.createElement("div")
-        nav_site_bar.setAttribute("class", "nav-site")
-        let nav_site_title = "<div class=\"nav-site-title\">" + category + "</div>"
-        let nav_site_content = "<div class=\"nav-site-content\">"
+        let navSiteBar = document.createElement("div")
+        navSiteBar.className = "nav-site"
+        let navSiteTitle = document.createElement("div")
+        navSiteTitle.className = "nav-site-title"
+        navSiteTitle.innerHTML = category
+        let navSiteContent = document.createElement("div")
+        navSiteContent.className = "nav-site-content"
         if (category.match(/no-\d/)) {
-            nav_site_title = ""
+            navSiteTitle = ""
         }
-        for (let site of NAVSITE[category]) {
-            nav_site_content += "<a href=\"" + site.href + "\"><img src=\"" + site.image + "\"><span>" + site.title + "</span></a>"
-        }
-        nav_site_bar.innerHTML = nav_site_title + nav_site_content + "</div>"
-        section.appendChild(nav_site_bar)
+        NAVSITE[category].forEach(site => {
+            navSiteContent.innerHTML += `
+                <a href="${site.href}">
+                    <img src="${site.image}">
+                    <span>${site.title}</span>
+                </a>
+            `
+        })
+        navSiteBar.append(navSiteTitle)
+        navSiteBar.append(navSiteContent)
+        section.appendChild(navSiteBar)
     }
 }
 
 function creatNav2() {
     let section = document.querySelector("section")
-    section.setAttribute("class", "nav-box")
+    section.className = "nav-box"
     for (let category in NAVSITE) {
         let nav = document.createElement("div")
         nav.setAttribute("class", "nav-site")
